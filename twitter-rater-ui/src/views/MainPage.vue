7923bc529cb8070e60d342af018cc426a53d205c
kannadan <joonas.sou@gmail.com>
2022-01-12 21:10:53 +0200

    profile view visible and loads tweets. Fixed api url for production

--- twitter-rater-ui/src/views/MainPage.vue
+++ twitter-rater-ui/src/views/MainPage.vue
@@ -1,7 +1,15 @@
 <template>
     <v-container fill-height fluid>
+        <v-row justify="center" align="top">
+            <v-col>
+                <h1>Twitter profile evaluator</h1>                
+
+            </v-col>
+
+        </v-row>
         <v-row align="center" justify="center">
             <v-col md="4">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
                 <v-text-field
                     v-model="searchTerm"
                     label="Profile name"
@@ -21,13 +29,14 @@
         name: "MainPage",
         data() {
             return {
-                searchTerm: ""
+                searchTerm: "",
+                apiUrl: process.env.VUE_APP_API_URL
             }
         },
         methods: {
             getProfile(){
                 console.log("Profile search " + this.searchTerm)
-                fetch("http://192.168.1.108:5000/api/GetProfile?username=" + this.searchTerm)
+                fetch(this.apiUrl + "GetProfile?username=" + this.searchTerm)
                     .then(response => response.json())
                     .then((data) => {
                         console.log(data)
--- twitter-rater-ui/src/views/ProfilePage.vue
+++ twitter-rater-ui/src/views/ProfilePage.vue
@@ -1,12 +1,75 @@
 <template>
-    <div>
-        Profile page
-    </div>
+    <v-container fluid align="start">
+        <v-row justify="center" align="start">            
+                <h1>{{ username }}</h1>                            
+        </v-row>
+        <v-row justify="center" class="mt-16" fill-height>
+            <v-col md=5 class="mt-6">
+                <v-card elevation="2" >
+                    <v-row>
+                        <v-img
+                        class="ma-4"
+                        max-height="250"
+                        max-width="250"
+                        :src="profile_pic"
+                        ></v-img>
+                        <v-col class="ma-4" align="left">
+                            <h3>Profile name</h3>         
+                            <div class="mt-8">
+                                <p>Profile desctiption here</p>                            
+                                <p>Other info about the profile here if there is any</p>                                
+
+                            </div>                                        
+                        </v-col>
+                    </v-row>
+                    <v-row class="mx-16">
+                        <v-col align="left">
+                            <h2>Score 1:</h2>
+                        </v-col>
+                        <v-col align="right">
+                            <h2>5.5</h2>
+                        </v-col>
+                    </v-row>
+                    <v-divider class="mx-16"></v-divider>
+                    <v-row class="mx-16">
+                        <v-col align="left">
+                            <h2>Score 2:</h2>
+                        </v-col>
+                        <v-col align="right">
+                            <h2>7.4</h2>
+                        </v-col>
+                    </v-row>
+                </v-card> 
+            </v-col>
+            <v-col md=5>
+                <div v-for="tweet in tweets" :key="tweet">
+                    <Tweet :id="tweet" :options="{conversation : 'None', cards: 'hidden'}"></Tweet>
+                </div>
+            </v-col>
+        </v-row>
+
+    </v-container>
 </template>
 
 <script>
+import { Tweet } from 'vue-tweet-embed'
     export default {
-        name: "ProfilePage"
+        name: "ProfilePage",
+        components: {
+            Tweet,
+        },
+        data() {
+            return {
+                username: "Test user",
+                profile_pic: require('@/assets/profile_default.jpg'),
+                tweets: [
+                    "1481256002741755905",
+                    "1481188837665742848",
+                    "1479475073241260036"
+                ]
+            }
+        },
+
     }
 </script>
 
