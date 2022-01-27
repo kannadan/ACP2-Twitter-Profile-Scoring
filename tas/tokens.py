6e7b678beb5cee78e70fcc2a3e1b0ed9b0473b2d
Joonas Soudunsaari <joonas.sou@gmail.com>
2022-01-27 18:47:18 +0200

    Delete tokens.py

--- tas/tokens.py
+++
@@ -1,5 +0,0 @@
-consumer_key = 'nzD0paNYtuF39hYwzcyuHtmSY'
-consumer_secret = 'Yg2Rb56i8GiliRNlrnGv28vf4pdWQBuVnM3ZavXrvBTIQdAqgr'
-access_token = '1463645210680905735-71Q1pqw4C0dVBXW47NJW2Qbzfio8n3'
-access_secret = 'eIF3Tfc98lHi1s82VOfmJKYNwBX2FqdI5yyWLQSpGPCAv'
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAAPOcWAEAAAAAR6wQ44tMHGldnH0QcOBnaENt4Ak%3DEEh02FAuchGAXzpqyVGxMggo2dUNVBtRPqU8LbC2mNQ92vAkQq'
