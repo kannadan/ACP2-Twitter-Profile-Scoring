31e8ed3772b2f28a3da5da97fed5896c51e907c1
Teemu Hannula <thannula@gmail.com>
2022-03-01 17:37:54 +0200

    mturk scripts updated

--- mturk/mturk-answers.py
+++ mturk/mturk-answers.py
@@ -5,11 +5,11 @@ import json
 import datetime
 import time
 from xml.dom.minidom import parseString
-from common.logger import logger
+
 
 def mturk_conf():
     keys = configparser.ConfigParser()
-    keys.read("keys.ini")
+    keys.read("own_keys.ini")
     region_name = keys.get("mturk", "region_name")
     aws_access_key_id = keys.get("mturk", "aws_access_key_id")
     aws_secret_access_key = keys.get("mturk", "aws_secret_access_key")
@@ -25,22 +25,50 @@ def mturk_conf():
     return client
 
 
-def mturk_result(mturk):
-    logger.info("Reading from hits.txt, writing to answers.json")
-    t = open("answers.json", "a")
-    logger.info(str(datetime.datetime.now()))
-    t.write('{"results %s": [' % (str(datetime.datetime.now())))
+def auto_approve_hits(client, assignment_id, process_time):
+    # Approve the Assignment (if it hasn't already been approved)
+    if process_time >= 15:
+        # 'Approving Assignment
+        client.approve_assignment(
+            AssignmentId=assignment_id,
+            RequesterFeedback='good',
+            OverrideRejection=False,
+        )
+    else:
+        # Rejecting Assignment
+        client.reject_assignment(
+            AssignmentId=assignment_id,
+            RequesterFeedback='auto reject, too short process time: %s s. Minimum is 15 s' % str(process_time),
+        )
+
+
+def mturk_result(mturk, hits_file):
+    # Reading from hits.csv, writing to answers.json
+    result_time = datetime.datetime.now().__str__()[:-7]
+    result_file = result_time.replace(".", "")
+    result_file = result_file.replace(" ", "_")
+    result_file = "answers_%s.json" % result_file.replace(":", "")
+    t = open(result_file, "a")
+    t.write('{"results %s": [' % result_time)
+    t.write("\n")
     first = True
-    with open("hits.txt", "r") as f:
+    with open(hits_file, "r") as f:
         for row_list in csv.reader(f):
-            time.sleep(1) # to prevent "ThrottlingException": Rate exceeded
+            time.sleep(1)  # to prevent "ThrottlingException": Rate exceeded
             response = mturk.list_assignments_for_hit(HITId=row_list[1], AssignmentStatuses=['Submitted', 'Approved'])
             assignments = response['Assignments']
-            logger.info('The number of submitted assignments for profile {} is {}'.format(row_list[0], len(assignments)))
+            # 'The number of submitted assignments for profile {} is {}'.format(row_list[0], len(assignments)))
+            x = 1
             for assignment in assignments:
                 worker_id = assignment['WorkerId']
                 assignment_id = assignment['AssignmentId']
                 answer_xml = parseString(assignment['Answer'])
+
+                accept_time = assignment['AcceptTime']
+                submit_time = assignment['SubmitTime']
+                time_difference = submit_time - accept_time
+                time_difference = time_difference.total_seconds()
+                assignment_status = assignment['AssignmentStatus']
 
                 # the answer is a xml document. we pull out the value of the first
                 # //QuestionFormAnswers/Answer/FreeText
@@ -51,10 +79,10 @@ def mturk_result(mturk):
                 dictionary = {
                     "twitter_ID": row_list[0],
                     "profile_score1": int(list(json_object["profile_score1"].keys())
-                                        [list(json_object["profile_score1"].values()).index(True)]),
+                                          [list(json_object["profile_score1"].values()).index(True)]),
                     "profile_score2": int(list(json_object["profile_score2"].keys())
-                                        [list(json_object["profile_score2"].values()).index(True)]),
-                    "tweet_score1":  int(list(json_object["tweet_score1"].keys())
+                                          [list(json_object["profile_score2"].values()).index(True)]),
+                    "tweet_score1": int(list(json_object["tweet_score1"].keys())
                                         [list(json_object["tweet_score1"].values()).index(True)]),
                     "tweet_score2": int(list(json_object["tweet_score2"].keys())
                                         [list(json_object["tweet_score2"].values()).index(True)]),
@@ -63,23 +91,23 @@ def mturk_result(mturk):
                     "HIT_ID": row_list[1],
                     "assignment_id": assignment_id,
                     "worker_id": worker_id,
-                    "assignments": len(assignments)
+                    "assignment count": x,
+                    "assignments": len(assignments),
+                    "assignment status": assignment_status,
+                    "accept time": accept_time.strftime("%Y-%m-%d, %H:%M:%S"),
+                    "submit time": submit_time.strftime("%Y-%m-%d, %H:%M:%S"),
+                    "assignment process time": time_difference
                 }
+                x += 1
                 if first:
                     first = False
                 else:
                     t.write(",\n")
 
                 t.write(json.dumps(dictionary))
-
-                # Approve the Assignment (if it hasn't already been approved)
                 if assignment['AssignmentStatus'] == 'Submitted':
-                    logger.info('Approving Assignment {}'.format(assignment_id))
-                    client.approve_assignment(
-                        AssignmentId=assignment_id,
-                        RequesterFeedback='good',
-                        OverrideRejection=False,
-                    )
+                    auto_approve_hits(mturk, assignment_id, time_difference)
+
     t.write("]}\n")
     t.close()
     f.close()
@@ -88,4 +116,5 @@ def mturk_result(mturk):
 
 if __name__ == "__main__":
     client = mturk_conf()
-    mturk_result(client)
+    mturk_result(client, "hits_2022-03-01_011727.csv")
+    # mturk_result(client, "hits.txt")
--- mturk/mturk-set.py
+++ mturk/mturk-set.py
@@ -1,12 +1,12 @@
 import boto3
 import time
+import datetime
 import configparser
-from common.logger import logger
 
 
 def mturk_conf():
     keys = configparser.ConfigParser()
-    keys.read("keys.ini")
+    keys.read("own_keys.ini")
     region_name = keys.get("mturk", "region_name")
     aws_access_key_id = keys.get("mturk", "aws_access_key_id")
     aws_secret_access_key = keys.get("mturk", "aws_secret_access_key")
@@ -22,7 +22,7 @@ def mturk_conf():
     return mturk_client
 
 
-def mturk_create_single_hit(ht1, ht2, ht3, ht4, ht5, line):
+def mturk_create_single_hit(ht1, ht2, ht3, ht4, ht5, line, group):
     # Create hit
 
     question = open(file='question2.xml', mode='r').read() % (ht1, ht2, ht3, ht4, ht5)
@@ -30,27 +30,33 @@ def mturk_create_single_hit(ht1, ht2, ht3, ht4, ht5, line):
     time.sleep(1)  # to prevent "ThrottlingException": Rate exceeded
     new_hit = client.create_hit(
         Title='How trustworthy Twitter profile is?',
-        Description='See twitter profile and tweets and select how trustworthy you think those are',
+        Description='See twitter profile and tweets. On scale 1 to 10, choose how much you trust those.',
         Keywords='text, quick, labeling, Twitter',
         Reward='0.10',
-        MaxAssignments=5,
-        # LifetimeInSeconds = 172800,
-        LifetimeInSeconds=900,
-        AssignmentDurationInSeconds=600,
-        AutoApprovalDelayInSeconds=14400,
+        MaxAssignments=5,                   # max assignment per hit
+        LifetimeInSeconds=172800,           # hit availability time 345600 - 4d, 900 - 15min
+        # LifetimeInSeconds=900,            # 172800 - 2 days
+        AssignmentDurationInSeconds=900,
+        AutoApprovalDelayInSeconds=345600 + group,
         Question=question,
         RequesterAnnotation=line,
     )
     return new_hit
 
 
-def mturk_hits(profilelist):
-    logger.info("Reading from list.txt, writing to hits.txt")
+def mturk_hits(profilelist, group_size):
+    # Reading from list.txt, writing to hits.csv
 
-    t = open("hits.txt", "a")
+    hits_time = datetime.datetime.now().__str__()[:-7]
+    hits_file = hits_time.replace(".", "")
+    hits_file = hits_file.replace(" ", "_")
+    hits_file = "hits_%s.csv" % hits_file.replace(":", "")
+
+    t = open(hits_file, "a")
+    group = 0
+    c1 = 0
     with open(profilelist, "r") as f:
         for line in f:
-            # for link in csv.reader(f):
             line = line.rstrip('\n')
             link = "https://mturk-critical.s3.eu-central-1.amazonaws.com/junhao-dataset/" + line
 
@@ -65,10 +71,13 @@ def mturk_hits(profilelist):
             ht4 = hyperlink_format.format(link=link + "_tweet_2.png", text="Tweet_2")
             ht5 = hyperlink_format.format(link=link + "_tweet_3.png", text="Tweet 3")
 
-            new_hit = mturk_create_single_hit(ht1, ht2, ht3, ht4, ht5, line)
+            new_hit = mturk_create_single_hit(ht1, ht2, ht3, ht4, ht5, line, group)
 
-            logger.info("https://workersandbox.mturk.com/mturk/preview?groupId=" + new_hit['HIT']['HITGroupId'])
-            logger.info("HITID = " + new_hit['HIT']['HITId'] + " (Use to Get Results)")
+            c1 += 1
+            if c1 > group_size:
+                group += 1
+                c1 = 0
+
             t.write(line + ",")
             t.write(new_hit['HIT']['HITId'] + ",")
             t.write(new_hit['HIT']['HITGroupId'] + ",")
@@ -80,5 +89,5 @@ def mturk_hits(profilelist):
 
 if __name__ == "__main__":
     client = mturk_conf()
-    # mturk_hits("random_100_idlist.txt")
-    mturk_hits("list.txt")
+    mturk_hits("random_100_idlist.txt", 110)
+    # mturk_hits("list.txt", 100)
